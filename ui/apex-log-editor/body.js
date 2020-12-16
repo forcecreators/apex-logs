ab59b20a65e916b51c763bca810555086f3c76bc
mjsmckenzie <matt@forcecreators.com>
2020-12-15 22:18:10 -0600

    Fix selected tab ui bug

--- ui/apex-log-editor/body.js
+++ ui/apex-log-editor/body.js
@@ -50,6 +50,8 @@ window.addEventListener("message", (event) => {
 $("#nav-tab a").on("click", function (e) {
     e.preventDefault();
     currentTab = $(this).attr("id");
+    console.log(currentTab);
+    if (currentTab !== "limits-tab") $(".active").removeClass("active");
     if (profile) renderTab(currentTab);
 });
 
